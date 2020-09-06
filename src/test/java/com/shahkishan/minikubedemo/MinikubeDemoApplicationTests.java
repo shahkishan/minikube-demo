fe93e67dbb871e9ae688ae03d97f6cb061b7a3bf
Kishan Shah <kshah1125@gmail.com>
2020-09-06 23:54:11 +0530

    Added controller and unit test

--- src/test/java/com/shahkishan/minikubedemo/MinikubeDemoApplicationTests.java
+++ src/test/java/com/shahkishan/minikubedemo/MinikubeDemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.shahkishan.minikubedemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.Assert;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MinikubeDemoApplicationTests {
 
+    @Autowired private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mockMvc.perform(get("/")).andExpect(result -> {
+            String response = result.getResponse().getContentAsString();
+            Assertions.assertEquals("Hello World", response);
+        });
+
+
     }
 
 }
